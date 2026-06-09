241412b1ab37bfcbe885c1d93f57c057442f1f5f
rodriguezemir <emir49499141@gmail.com>
2026-06-08 22:03:08 -0300

    init commit

---
+++ src/main/java/me/rodriguezemir/school_panel/SchoolPanelApplication.java
@@ -0,0 +1,13 @@
+package me.rodriguezemir.school_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/rodriguezemir/school_panel/SchoolPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rodriguezemir.school_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
